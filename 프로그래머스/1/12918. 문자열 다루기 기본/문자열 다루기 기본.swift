3d49d8ac90e2bf5c32fa5ebbaabcff2be8f7d85f
hello22433 <104301546+hello22433@users.noreply.github.com>
2023-11-02 16:44:12 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -0,0 +1,13 @@
+func solution(_ s:String) -> Bool {
+    
+    if (s.count != 4 && s.count != 6) {
+        return false
+    } else {
+            if (Int(s) != nil) {
+                return true
+            } else {
+                return false
+            }
+        } 
+        return true
+    }
