aba0f5c380a88874edd050d2f487ea86f049de00
hello22433 <104301546+hello22433@users.noreply.github.com>
2023-11-03 14:47:33 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ number:[Int]) -> Int {
+    var count = 0
+    
+    for i in 0..<number.count {
+        if (number.count - i < 3) {
+            break;
+        }
+        for j in i+1..<number.count {
+            for k in j+1..<number.count {
+                if (number[i] + number[j] + number[k] == 0) {
+                    count += 1
+                }
+                
+            }
+        }
+    }
+    return count
+}
