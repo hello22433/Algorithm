20b168a0cef425a9296350761b49784febbcccbf
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-24 12:21:25 +0900

    [Bronze V] Title: N 찍기, Time: 852 ms, Memory: 32616 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+
+        for (long i = 1; i <= a; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+//
