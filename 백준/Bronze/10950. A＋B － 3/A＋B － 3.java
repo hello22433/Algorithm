2c25903cc753984f3622bee3ae1c2bd40503a55a
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-24 12:23:49 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long answer = 1;
+
+        for (long i = 1; i <= a; i++) {
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            System.out.println(b + c);
+        }
+    }
+}
+
+//
