837c179449840291a51af4dbe2b022ba93e8bed7
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-24 11:39:19 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a - b);
+    }
+}
+
+//
