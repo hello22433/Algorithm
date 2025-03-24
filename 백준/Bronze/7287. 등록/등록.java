ed690f5ef965adeb11de6602e570b209997f8548
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-24 11:26:03 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 맞은 문제의 수
+        int solvedProblems = 16;
+        
+        // 아이디
+        String id = "hello2243";
+        
+        // 출력
+        System.out.println(solvedProblems);
+        System.out.println(id);
+    }
+}
