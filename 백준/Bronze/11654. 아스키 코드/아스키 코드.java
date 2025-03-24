d5c41d553d8b6afa807446a5ecfe964ad9422bd5
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-24 18:50:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.nio.Buffer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.next().charAt(0);
+        System.out.println((int)c);
+
+    }
+}
+
+//
