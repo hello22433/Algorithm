b040c404b064cc8d4091e75efa4f6e064ef62fe5
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-25 10:09:50 +0900

    [Bronze V] Title: 이상한 기호, Time: 172 ms, Memory: 17680 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.nio.Buffer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] ab = scanner.nextLine().split(" ");
+        int A = Integer.parseInt(ab[0]);
+        int B = Integer.parseInt(ab[1]);
+        System.out.print(calcalator(A,B));
+    }
+
+    private static int calcalator(int A, int B) {
+        return (A+B)*(A-B);
+    }
+}
+
+// 티셔츠, 연필 모두 최소를 맞춰야 한다. 연필은 7로 나눈 몫 + 나머지
+// 티셔츠는 각각에 대해 나누고 몫 + 나머지가0이아니라면 +1
