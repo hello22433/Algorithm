9f7ee24bbce75422535aebcb3d301f0b40116acf
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-03-26 18:46:23 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 53448 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,34 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.Buffer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            int poller = q.poll();
+            q.offer(poller);
+        }
+
+        System.out.print(q.poll());
+    }
+}
+
+// 정렬 하고 투포인터로 같이 비교한다.
+//
+
