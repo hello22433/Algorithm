b3411ce7796afaa7a9452c73043baf86a59f4f58
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-04-07 13:31:04 +0900

    [Gold V] Title: 강의실 배정, Time: 604 ms, Memory: 68788 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,7 +1,10 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 
 public class Main {
 
@@ -19,18 +22,29 @@ public class Main {
             matrix[i][1] = T;
         }
 
-        Arrays.sort(matrix, (a, b) -> a[0] - b[0]); // 시작 시간 기준 정렬
+        Arrays.sort(matrix, (a,b) -> {
+            return a[0] - b[0];
+        });
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(matrix[0][1]); // 첫 회의 끝나는 시간 넣기
+        pq.add(matrix[0][1]);
 
+        int answer = 1;
         for (int i = 1; i < N; i++) {
             if (pq.peek() <= matrix[i][0]) {
-                pq.poll(); // 겹치지 않으니 기존 회의실 사용
+                pq.poll();
             }
-            pq.add(matrix[i][1]); // 새 회의 종료 시간 넣기
+            pq.add(matrix[i][1]);
         }
 
-        System.out.println(pq.size()); // 필요한 회의실 수
+        System.out.println(pq.size());
     }
 }
+
+// 끝나는 시간을 기준으로 오름차순 정렬한다.(더 빨리 끝나는 순)
+// 끝나는 시간이 같으면 시작시간이 더 빠른 걸로 정렬한다. 어차피 처리해야 할 것이었기 때문.
+//
+// 우선순위 큐에 끝나는 시간만 저장한다.
+// 만약 peek보다 다음 시작시간이 더 작다면 큐에다가 넣는다.
+// 아니라면 poll하고 끝나는 시간을 큐에다가 넣는다.
+// 매번 마지막에 큐의 사이즈를 max로 갱신한다.
