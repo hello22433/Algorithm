edcfda7f12655acaa2858ccc4243b8852ac45475
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-04-19 00:31:44 +0900

    [Silver III] Title: 퇴사, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,45 +1,41 @@
-
 import java.util.*;
 
 public class Main {
     static int N;
-    static int[][] Narr;
+    static int[][] arr;
     static int result = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        Narr = new int[N+1][2];
-
-        for (int i = 1; i <= N; i++) {
-            Narr[i][0] = sc.nextInt(); // Ti
-            Narr[i][1] = sc.nextInt(); // Pi
+        arr = new int[N+1][2];
+        for (int i = 1; i < N+1; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
         }
-        
-        dfs(1,0);
 
-        System.out.println(result);
+        dfs(1, 0);
+        
+        System.out.print(result);
     }
 
-    private static void dfs(int day, int sum) {
+    private static void dfs(int day, int PiSum) {
         if (day == N+1) {
-            result = Math.max(result, sum);
+            result = Math.max(result, PiSum);
             return;
         }
-        
-        if (day + Narr[day][0] <= N+1) {
-            dfs(day+Narr[day][0], sum+Narr[day][1]);
+
+        // 상담을 하는 경우
+        if (day+arr[day][0] <= N + 1) {
+            dfs(day + arr[day][0], PiSum + arr[day][1]);
         }
-       
-        dfs(day+1, sum);
+        
+        // 상담을 안 하는 경우
+        dfs(day+1, PiSum);
     }
 }
 
 
 
-// 하루에 하나씩 서로 다른 사람의 상담을 잡아놓았다.
-// Ti-1만큼 앞으로는 상담을 할 수 없다.
-// i=0 i<N 까지 인덱스에 각각을 배열로 저장한다.
-// Ti만큼 i에 더한다.
-// sum+= Pi.
-// 만약 i가 N-1을 넘어가면(인덱스 최대) return하고 result=Math.max(sum,result)
+// 상담을 하였을때 if (i+Narr[0] <= N + 1) 해당시간을 더해서 넘겨준다.
+// i가 N+1일때 멈추고 max갱신한다.
+// 상담을 안 하는 경우는 i에 1만을 더해서 넘겨준다.
