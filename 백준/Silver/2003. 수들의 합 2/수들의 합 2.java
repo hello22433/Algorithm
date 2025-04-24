daf34d8c0cd3b57fc007534dec2799b8e18e6eb7
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-04-24 14:29:48 +0900

    [Silver IV] Title: 수들의 합 2, Time: 404 ms, Memory: 30412 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,34 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = i; j < N; j++) {
+                sum += arr[j];
+                if (sum == M) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
+
+
+
+// 1씩 더한다.
