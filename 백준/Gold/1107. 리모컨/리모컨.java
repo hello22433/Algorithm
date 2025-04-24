5c260cb177eb4ffbbc2b29634621a820a6578d34
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-04-24 15:11:38 +0900

    [Gold IV] Title: 리모컨, Time: 360 ms, Memory: 69980 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,4 +1,5 @@
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -6,36 +7,37 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 목표 채널
-        int M = sc.nextInt(); // 고장난 버튼 수
 
-        Set<Integer> brokenButtons = new HashSet<>();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        Set<Integer> arr = new HashSet<>();
         for (int i = 0; i < M; i++) {
-            brokenButtons.add(sc.nextInt());
+            arr.add(sc.nextInt());
         }
+        min = Math.min(min, Math.abs(100-N));
 
-        // 방법 1: + 또는 -만 사용
-        min = Math.abs(100 - N);
-
-        // 방법 2: 숫자 버튼으로 만들 수 있는 채널을 전수조사
         for (int i = 0; i <= 1000000; i++) {
-            String str = Integer.toString(i);
-            boolean isValid = true;
+            String iStr = Integer.toString(i);
 
-            for (int j = 0; j < str.length(); j++) {
-                int digit = str.charAt(j) - '0';
-                if (brokenButtons.contains(digit)) {
-                    isValid = false;
+            for (int j = 0; j < iStr.length(); j++) {
+                if (arr.contains(Character.getNumericValue(iStr.charAt(j)))) {
                     break;
+                }
+                if (j == iStr.length() - 1) {
+                    min = Math.min(min, Math.abs(i - N) + iStr.length());
                 }
             }
 
-            if (isValid) {
-                int pressCount = str.length() + Math.abs(i - N); // 숫자 입력 + +/- 이동
-                min = Math.min(min, pressCount);
-            }
         }
 
         System.out.println(min);
+
     }
 }
+
+
+
+
+// 100부터 시작!
+// +-로 최솟값을 구할 수 있는지 확인
+// N
