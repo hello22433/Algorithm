cd469761df642e38bf8aff05c34138d88016a256
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-11 21:00:08 +0900

    Title: 일렬로 서있는 소 2, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250511/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        // Please write your code here.
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if (a[i] <= a[j] && a[j] <= a[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
