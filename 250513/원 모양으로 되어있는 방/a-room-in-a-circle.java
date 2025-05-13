55675d92608ff5b3d89f8f29823f9e4f4dfe536d
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-13 13:42:02 +0900

    Title: 원 모양으로 되어있는 방, Time: 206ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250513/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // Please write your code here.
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int bucketDistanceSum = 0;
+            int distance = 0;
+            for (int j = i; j < n + i; j++) {
+                bucketDistanceSum += arr[j%n] * distance++;
+            }
+            minDistance = Math.min(minDistance, bucketDistanceSum);
+        }
+
+        System.out.print(minDistance);
+    }
+}
+
+// 하나의 방을 정한다 -> 하나의 방의 전 방까지 순회한다.
+// %(N+1) 
+// i로 순회하면서 하나씩 방을 정한다 (인덱스로). 이중탐색으로 i부터 시작하여 j < n+i(i가 돌아간 만큼 더 돌아가야 하므로)
+// 
