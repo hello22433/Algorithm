a25533b708e273c2943effa535231ab5fc891ad1
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-13 13:14:22 +0900

    Title: 이상한 진수 2, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250513/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        // Please write your code here.
+        StringBuilder sb = new StringBuilder();
+        boolean changed = false;
+        for (int i = 0; i < a.length(); i++) {
+            if (!changed && a.charAt(i)=='0') {
+                changed = true;
+                sb.append("1");
+            } else {
+                sb.append(String.valueOf(a.charAt(i)));
+            }
+        }
+        String resultStr = sb.toString();
+
+        int num = 0;
+        for (int i = 0; i < resultStr.length(); i++) {
+            num = num * 2 + Character.getNumericValue(resultStr.charAt(i));
+        }
+
+        System.out.print(num);
+    }
+}
+
+// a에서 숫자를 하나씩 바꿔서 10진법으로 나타낸다.
+// 
