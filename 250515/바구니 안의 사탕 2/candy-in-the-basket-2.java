1f712055b64d066b24dacf06d7fdfa8e09b43a25
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-15 12:56:33 +0900

    Title: 바구니 안의 사탕 2, Time: 144ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250515/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        // int[] candies = new int[n];
+        // int[] positions = new int[n];
+        candyAndPosition[] candies = new candyAndPosition[n];
+        for (int i = 0; i < n; i++) {
+            candies[i] = new candyAndPosition(sc.nextInt(), sc.nextInt());
+            // candies[i] = sc.nextInt();
+            // positions[i] = sc.nextInt();
+        }
+
+        // 0~100
+        int[] line = new int[101];
+        for (int i = 0; i < n; i++) {
+            candyAndPosition candyInfo = candies[i];
+            line[candyInfo.position] = candyInfo.candies;
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = c; i <= 101-c-1; i++) {
+            int sum = 0;
+            for (int j = i-c; j <= i+c; j++) {
+                sum += line[j];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        System.out.print(maxSum);
+    }
+}
+
+class candyAndPosition {
+    int candies;
+    int position;
+
+    candyAndPosition(int candies, int position) {
+        this.candies = candies;
+        this.position = position;
+    }
+}
