7e4aa46455f9ca3c07560f1e6203503efdf54d09
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-20 21:46:04 +0900

    Title: 개발자의 순위, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250520/개발자의 순위/developers-rank.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] ranking = new int[k][n];
+        int[][] makeIndividualRanking = new int[n+1][n+1];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                ranking[i][j] = sc.nextInt();
+                makeIndividualRanking[ranking[i][j]][j+1] += 1;
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n+1; i++) {
+            int minRank = Integer.MAX_VALUE; 
+            for (int j = 0; j < n+1; j++) {
+                if (makeIndividualRanking[i][j] != 0) minRank = Math.min(minRank, n-j);
+            }
+            if (minRank != Integer.MAX_VALUE) sum += minRank;
+        }
+        System.out.print(sum);
+    }
+}
+
+// int[N+1][N+1] makeIndividualRanking를 만든다. 1부터 N명 각각의 순위를 기록하는 배열이다.
+// ranking[i][j]... makeIndividualRanking[ranking[i][j]][j+1] += 1;
+// makeIndividualRanking[i][j] 을 돌면서  값이 0이 아니라면 n-j값으로 최소값을 갱신한다.(i 돌때마다 리뉴얼)
+// 더한다.
+// 
