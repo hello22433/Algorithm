446e68f56bd9a37f5318f4faddeeb4ade2968feb
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-21 12:43:34 +0900

    Title: 스승의 은혜 3, Time: 274ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 250521/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -16,6 +16,13 @@ public class Main {
 
         Arrays.sort(psBox, (a,b) -> {
             
+            // 같을떄? 크거나 같을떄? 
+            // 토탈 비용으로 오름차순할 건데,,, 동시에 상품 가격으로 오름차순하는게 맞다.
+            // 토탈비용이 더 높더라도 상품 가격이 더 높아서 할인했을 때 구매가 가능할 수 있다. 
+            // 그럼 구매가 불가할 때 상품 가격이 더 높은 것을 고려해야겠지.
+            // 그래서 같은 가격이라면 배송비로 오름차순(상품가격 내림차순) 한거니까..
+            // 근데 같은 가격이 아니라 가격이 더 비싼 경우에도 고려해야 한다.
+            // 남은 것들 중에 모두 고려해보면 되지 않을까?
             if (a[0]+a[1] == b[0]+b[1]) {
                 return a[1]-b[1];
             }
@@ -24,7 +31,7 @@ public class Main {
         });
 
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
+        outer : for (int i = 0; i < n; i++) {
             int presentPrice = psBox[i][0];
             int deliveryFee = psBox[i][1];
             int TotalPrice = presentPrice + deliveryFee;
@@ -33,11 +40,18 @@ public class Main {
                 B -= TotalPrice;
                 cnt++;
             } else {
-                TotalPrice /= 2;
+                for (int j = i; j < n; j++) {
+                    presentPrice = psBox[j][0];
+                    deliveryFee = psBox[j][1];
+                    TotalPrice = presentPrice + deliveryFee;
 
-                if (B - TotalPrice >= 0) {
-                    cnt++;
+                    presentPrice /= 2;
+                    if (B - (presentPrice + deliveryFee) >= 0) {
+                        cnt++;
+                        break outer;
+                    }
                 }
+
                 break;
             }
         }
