5db090d848ef2dc821797c66efa3d18cb9ce8359
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-21 14:59:26 +0900

    Title: 팰린드롬 수 찾기, Time: 157ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250521/팰린드롬 수 찾기/find-the-number-of-palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        // Please write your code here.
+
+        int cnt = 0;
+        for (int i = x; i <= y; i++) {
+            String strI = String.valueOf(i);
+
+            boolean check = false;
+            for (int j = 0; j < strI.length(); j++) {
+                int frontChar = strI.charAt(j);
+                int backChar = strI.charAt(strI.length() - 1 - j);
+
+                if (frontChar != backChar) {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
+
+// 
