d33236efa2251b291dcf2c46c8fdad73ba131db7
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-22 13:20:44 +0900

    Title: 3개의 선 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250522/3개의 선 2/three-lines-2.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        if (n <= 3) {
+            System.out.print(1);
+            return;
+        }
+ 
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    int YLine1 = x[i];
+                    int XLine1 = y[i];
+                    int YLine2 = x[j];
+                    int XLine2 = y[j];
+                    int YLine3 = x[k];
+                    int XLine3 = y[k];
+
+                    int[] whatLine = {YLine1, XLine1, YLine2, XLine2, YLine3, XLine3};
+                    
+                    for (int a = 0; a < 2; a++) {
+                        for (int b = 2; b < 4; b++) {
+                            for (int c = 4; c < 6; c++) {
+                                Set<Integer> xSet = new HashSet<>();
+                                Set<Integer> ySet = new HashSet<>();
+
+                                if (a % 2 == 0) {
+                                    xSet.add(whatLine[a]);
+                                } else {
+                                    ySet.add(whatLine[a]);
+                                }
+
+                                if (b % 2 == 0) {
+                                    xSet.add(whatLine[b]);
+                                } else {
+                                    ySet.add(whatLine[b]);
+                                }
+
+                                if (c % 2 == 0) {
+                                    xSet.add(whatLine[c]);
+                                } else {
+                                    ySet.add(whatLine[c]);
+                                }
+                                
+                                // 모든 점에서 해당 점선이 적용되는지 확인
+                                for (int q = 0; q < n; q++) {
+                                    for (int w = q+1; w < n; w++) {
+                                        for (int e = w+1; e < n; e++) {
+                                            if ((xSet.contains(x[q]) || ySet.contains(y[q])) && (xSet.contains(x[w]) || ySet.contains(y[w])) && (xSet.contains(x[e]) || ySet.contains(y[e]))) {
+                                                System.out.print(1);
+                                                return;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                
+                            }
+                        }
+                    }
+                    
+
+                }
+            }
+        }
+
+
+        System.out.print(0);
+    }
+}
+
+// 점마다 x,y축을 나눠 선을 놓아본다.
+//
+// 점을 세 개 고른다
+// 점마다 선을 놓는다. 
+// 선마다 xy를 바꿔본다 (int XLine = y, int YLine = x)
+// 다른 점들의 y값이 Xline과 일치하거나 x값이 YLine과 일치하면 count
+// 모두 일치하면 정답이므로 출력하고 리턴.
+
+// 모든 점들이 직선에 포함되는지 확인한다.
+// 
