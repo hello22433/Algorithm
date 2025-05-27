b5b44ac5b53d0475d35f1d44109ec282e490dff0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-27 13:21:43 +0900

    Title: 살 수 있는 물건, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250527/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a>= 3000) {
+            System.out.print("book");
+        } else if (a>=1000) {
+            System.out.print("mask");
+        } else {
+            System.out.print("no");
+        }
+
+        
+    }
+}
