6d52d46a94c0b5b9e800a376faa0071951949442
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-27 19:46:34 +0900

    Title: 100점 도달하기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250527/100점 도달하기/reach-100-point.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc= new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int s = a; s < 100; s++) {
+            if (s >= 90) {
+                System.out.print("A ");
+            } else if (s >= 80) {
+                System.out.print("B ");
+            } else if (s >= 70) {
+                System.out.print("C ");
+            } else if (s >= 60) {
+                System.out.print("D ");
+            } else {
+                System.out.print("F ");
+            }
+        }
+
+        
+    }
+}
