2ba38404fd65a9d43f2f1a2b51d95aa3e20e0408
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-27 18:47:33 +0900

    Title: 윤년인가, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250527/윤년인가/is-leap-year.java
+++ 250527/윤년인가/is-leap-year.java
@@ -7,7 +7,7 @@ public class Main {
 
         int a = sc.nextInt();
         
-        if (a % 4 ==0 || (a % 100==0 && a % 400 != 0)) {
+        if (a % 4 ==0 && !(a % 100==0 && a % 400 != 0)) {
             System.out.print("true");
         } else {
             System.out.print("false");
