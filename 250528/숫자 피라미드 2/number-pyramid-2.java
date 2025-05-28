1a41a0b54938946a1ecc4867dc37cb5e377f1343
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-28 13:08:03 +0900

    Title: 숫자 피라미드 2, Time: 178ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250528/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int totalCnt = 1;
+
+        for (int i = 1; i <= N; i++) {
+            
+            for (int j  = 1; j <= i; j++) {
+                System.out.print(totalCnt++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
