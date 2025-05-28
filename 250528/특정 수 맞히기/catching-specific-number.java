8bf7ddb7c012c71408bdf47050da9941137ecbc7
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-28 11:11:42 +0900

    Title: 특정 수 맞히기, Time: 3075ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

---
+++ 250528/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+
+        while (A!=25) {
+            if (A>25) {
+            System.out.println("Higher");
+
+            } else {
+            System.out.println("Lower");
+
+            }
+        }
+        System.out.print("Good");
+    }
+}
