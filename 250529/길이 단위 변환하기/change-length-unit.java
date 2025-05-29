378bed2bc2f568057b775faa2bc1ef417b624a4c
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 11:47:30 +0900

    Title: 길이 단위 변환하기, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

--- 250529/길이 단위 변환하기/change-length-unit.java
+++ 250529/길이 단위 변환하기/change-length-unit.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.printf("9.2ft = %.1f\n1.3mi = %.1f", 9.2*30.48, 160934*1.3);
+        System.out.printf("9.2ft = %.1fcm\n1.3mi = %.1fcm", 9.2*30.48, 160934*1.3);
     }
 }
