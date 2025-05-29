7003ef45f6acaae93076cb4df9dce3283490eb39
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 12:33:05 +0900

    Title: 간단한 사칙연산, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250529/간단한 사칙연산/simple-arithmetic-operation.java
+++ 250529/간단한 사칙연산/simple-arithmetic-operation.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.next();
-        int b = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a/b);
