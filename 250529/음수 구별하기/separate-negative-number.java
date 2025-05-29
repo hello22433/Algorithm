3a114d9c81d3fea83c8bfccd6c98dad09e17f514
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 13:51:01 +0900

    Title: 음수 구별하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250529/음수 구별하기/separate-negative-number.java
+++ 250529/음수 구별하기/separate-negative-number.java
@@ -5,5 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a);
+        if (a<0) {
+            System.out.println("minus");
+        }
     }
 }
