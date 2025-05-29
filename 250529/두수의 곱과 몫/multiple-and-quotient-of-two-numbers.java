51cf6e8e672b43f989d0e735fd770c0fe670adf6
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 12:35:58 +0900

    Title: 두수의 곱과 몫, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250529/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
+++ 250529/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.printf("%d * %d = %d",a,b,a*b);
+        System.out.printf("%d * %d = %d\n",a,b,a*b);
         System.out.printf("%d / %d = %d",a,b,a/b);
     }
 }
