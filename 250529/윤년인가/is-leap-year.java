983b9329fa731af6550373c1ea161a41f13a8b61
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 14:55:08 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250529/윤년인가/is-leap-year.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+
+        if (s %4 == 0 && (s%100 && s%400!=0)) {
+            System.out.print("true");
+        }
+        else {
+            System.out.print("false");
+        }
+    }
+}
