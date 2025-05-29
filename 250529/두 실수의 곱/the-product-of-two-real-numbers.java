41180587605ea88c5901d3e81855688b1fccc94e
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 11:49:07 +0900

    Title: 두 실수의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250529/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5.26;
+        int b = 8.27;
+
+        System.out.printf("%.2f", a*b);
+    }
+}
