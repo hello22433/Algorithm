d0fd724976205d2931ce9b9139660a8b27d108e6
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-29 12:02:39 +0900

    Title: 점수 입출력, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250529/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // 추가 코드 작성
+
+        int a = sc.nextInt();
+        System.out.printf("Your score is %d point",a);
+    }
+}
