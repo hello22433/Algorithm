966754480f7343cc84b9a2b31bcf9b7df5d73424
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 15:21:11 +0900

    Title: 0 나올때까지 출력, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250531/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        int sumVal = 0;
+        while (true) {
+            n = sc.nextInt();
+            if (n == 0) return;
+            
+            System.out.println(n);
+        }
+    }
+}
