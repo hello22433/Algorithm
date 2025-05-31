dc5ca9eae5d4e67c255603c52845817c4941fa3b
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 13:29:07 +0900

    Title: 홀수만 출력 2, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250531/홀수만 출력 2/output-only-odd-2.java
+++ 250531/홀수만 출력 2/output-only-odd-2.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
         int b = sc.nextInt();
+        int a = sc.nextInt();
         
-        for (int i = a; i >= b; i--) {
+        for (int i = b; i >= a; i--) {
+            if (i % 2 != 0)
             System.out.print(i + " ");
         }
     }
