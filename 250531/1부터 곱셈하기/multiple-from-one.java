9e7c807b0e1307e0e916116b5e2c136bd04b6972
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 15:00:47 +0900

    Title: 1부터 곱셈하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250531/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+
+        int sumVal = 1;
+        int cnt = 0;
+        for (int i = 1; i <= 10; i++) {
+
+            if (sumVal * i >= a) {
+                System.out.print(i-1);
+                break; 
+            }
+
+            sumVal *= i;
+            
+        }
+        
+        
+
+    }
+}
+
+// 
