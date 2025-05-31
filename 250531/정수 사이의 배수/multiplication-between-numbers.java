6527f44d044a5b6948aa66198d13d71d570cbb7c
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 14:32:03 +0900

    Title: 정수 사이의 배수, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250531/정수 사이의 배수/multiplication-between-numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sumVal = 0;
+        for (int i = a; i <= b; i++) {
+            
+            if (i % 5 == 0 || i% 7 == 0)
+            sumVal += i;
+            
+        }
+
+        System.out.print(sumVal + sumVal/(double)(b-a));
+
+    }
+}
+
+// 
