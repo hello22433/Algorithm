bb989c270e4f4b781046529075e183e916cd10ca
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 14:29:27 +0900

    Title: 등장횟수 확인, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250531/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int c = 0;
+        for (int i = 1; i <= 5; i++) {
+            
+            if (i % 2 ==0) {
+                c +=1;
+            } 
+            
+        }
+
+        System.out.print(c);
+
+    }
+}
+
+// 
