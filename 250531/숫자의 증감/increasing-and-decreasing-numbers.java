30e7e117fe0aa9d7f4139da01c18265d4df4182c
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 13:55:05 +0900

    Title: 숫자의 증감, Time: 125ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250531/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        int n = sc.nextInt();
+
+        if (c == 'A') {
+            for (int i = 1; i <= n; i++) {
+                System.out.println(i);
+            }
+        } else if (c == 'D') {
+            for (int i = n; i >= 1; i--) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
