954a991d09c35786730c1ac3df6ff8f9e552a3fa
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 15:00:19 +0900

    Title: 1부터의 합, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250531/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+
+        int sumVal = 0;
+        int cnt = 0;
+        for (int i = 1; i <= 100; i++) {
+
+            if (sumVal + i >= a) {
+                System.out.print(i);
+                break; 
+            }
+
+            sumVal += i;
+            
+        }
+        
+        
+
+    }
+}
+
+// 
