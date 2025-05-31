56e24631c1803913f139f271dc3b9a238abb5a96
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-05-31 14:37:06 +0900

    Title: 정수 사이의 배수, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250531/정수 사이의 배수/multiplication-between-numbers.java
+++ 250531/정수 사이의 배수/multiplication-between-numbers.java
@@ -10,9 +10,11 @@ public class Main {
         int cnt = 0;
         for (int i = a; i <= b; i++) {
             
-            if (i % 5 == 0 || i% 7 == 0)
-            cnt++;
+            if (i % 5 == 0 || i% 7 == 0) {
+cnt++;
             sumVal += i;
+            }
+            
             
         }
 
