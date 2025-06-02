fcccaa74f9fae203f788807104f980210ef1e83e
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-02 11:34:06 +0900

    Title: 정사각형 두 개 출력, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250602/정사각형 두 개 출력/output-two-rectangles.java
+++ 250602/정사각형 두 개 출력/output-two-rectangles.java
@@ -11,6 +11,7 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
