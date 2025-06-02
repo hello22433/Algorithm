a001d22179bd8ae6fb0fa110a9c3599e016a7816
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-02 16:31:19 +0900

    Title: 좌우 지그재그 출력, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250602/좌우 지그재그 출력/print-lr-zigzag.java
+++ 250602/좌우 지그재그 출력/print-lr-zigzag.java
@@ -6,12 +6,28 @@ public class Main {
         int n = sc.nextInt();
 
 
+        int[][] arr = new int[n][n];
         int cnt = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.printf("%d ", cnt++);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <  n; j++) {
+                arr[i][j] = cnt++;
             }
-            System.out.println();
+            
+        }
+
+        for (int i = 0; i < n; i++) {
+            
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    System.out.printf("%d ", arr[i][j]);
+                }
+            } else {
+                for (int j = n-1; j >= 0; j--) {
+                    System.out.printf("%d ", arr[i][j]);
+                }
+            }
+            
+            
         }
 
 
