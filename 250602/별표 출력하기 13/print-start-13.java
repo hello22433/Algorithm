73fca5e1e22d32c2e23b28e97153bed1b3a383b7
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-02 12:41:32 +0900

    Title: 별표 출력하기 13, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250602/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = n; j >= i; j--) {
+                sb.append("* ");
+            }
+            
+            list.add(sb.toString());
+            sb = new StringBuilder();
+            for (int j = 0; j < i; j++) {
+                sb.append("* ");
+            }
+            list.add(sb.toString());
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
