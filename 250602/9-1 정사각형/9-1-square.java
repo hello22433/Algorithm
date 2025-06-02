2aee07fb7f17b176394fe7465fc64785b7c885ea
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-02 20:29:39 +0900

    Title: 9-1 정사각형, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250602/9-1 정사각형/9-1-square.java
+++ 250602/9-1 정사각형/9-1-square.java
@@ -8,7 +8,7 @@ public class Main {
         int cnt = 9;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                System.out.printf("%d ",cnt--);
+                System.out.printf("%d",cnt--);
                 if (cnt == 0) cnt=9;
             }
             System.out.println();
