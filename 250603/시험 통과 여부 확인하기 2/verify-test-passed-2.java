37beb0e5b91489312eb9e1fc99a1b37c84040474
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-03 14:55:47 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250603/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = nextInt();
+
+        double[] arr = new double[n];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+        double sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            // arr[i] = sc.nextDouble();
+            int sum == 0;
+            for (int j = 0; j < 4; j++) {
+                sum += sc.nextInt();
+            }
+
+            if (sum / 4 >= 60) {
+                System.out.println("pass");
+                cnt++;
+            } else {
+                System.out.println("fail");
+            }
+            
+        }
+
+        System.out.print(cnt);
+    }
+}
