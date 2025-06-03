5379f657415de96f3d4360d2979a510b6544d1d9
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-03 14:48:22 +0900

    Title: 배열 채우기, Time: 152ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 250603/배열 채우기/filling-array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        double[] arr = new double[10];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+        double sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextDouble();
+            if (arr[i] == 0) break;
+            sum += arr[i];
+            cnt++;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            if (arr[i] != 0) System.out.printf("%d ", arr[i]);
+        }
+    }
+}
