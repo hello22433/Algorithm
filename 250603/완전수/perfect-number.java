7f27a033fc3ec8efec116b72ec5595e6e4cbdacb
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-03 14:19:49 +0900

    Title: 완전수, Time: 149ms, Memory: 10MB, Status: Passed - Codetree

--- 250603/완전수/perfect-number.java
+++ 250603/완전수/perfect-number.java
@@ -10,7 +10,7 @@ public class Main {
         for (int i = start; i < end+1; i++) {
            
             int sum = 0;
-            for(int j = 2; j < i; j++) {
+            for(int j = 1; j < i; j++) {
                 if (i % j == 0) sum += j;
             }
             
