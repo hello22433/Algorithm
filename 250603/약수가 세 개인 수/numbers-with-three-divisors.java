617f0cedb6990be6495abbef411641720c028fb7
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-03 14:20:15 +0900

    Title: 약수가 세 개인 수, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250603/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 250603/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -11,7 +11,7 @@ public class Main {
            
             int sum = 0;
             for(int j = 1; j <= i; j++) {
-                if (i % j == 0) sum += j;
+                if (i % j == 0) sum++;
             }
             
             if (sum == 3) cnt++;
