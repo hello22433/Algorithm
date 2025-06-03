dae1e8c505e58a464a06439fe9c203f791a5f19d
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-03 14:23:26 +0900

    Title: 소수 구하기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 250603/소수 구하기/get-prime.java
+++ 250603/소수 구하기/get-prime.java
@@ -8,7 +8,7 @@ public class Main {
         outer : for (int i = 2; i < n+1; i++) {
            
             for(int j = 2; j < i; j++) {
-                if (i % j != 0) continue outer;
+                if (i % j == 0) continue outer;
             }
             
             System.out.print(i + " ");
