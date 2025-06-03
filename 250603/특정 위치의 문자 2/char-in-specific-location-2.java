3565e16f649f30e002bd970befd390f960040fdd
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-03 15:33:12 +0900

    Title: 특정 위치의 문자 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250603/특정 위치의 문자 2/char-in-specific-location-2.java
+++ 250603/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[10];
+        String[] arr = new String[10];
 
         for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = sc.next();
             if (i == 1 || i == 4 || i == 7) {
                 System.out.print(arr[i]);
             }
