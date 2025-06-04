52cafe6300294a46c19e0e8af920f4ae0b9c853b
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 10:21:36 +0900

    Title: 배열 놀이, Time: 159ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250604/배열 놀이/play-with-array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         
+
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        outer : for (int i = 0; i < q; i++) {
+            int command = sc.nextInt();
+
+            if (command == 1) {
+                int a = sc.nextInt();
+                System.out.println(arr[a]);
+            } else if (command == 2) {
+                int b = sc.nextInt();
+                for (int j = 0; j < n; j++) {
+                    if (arr[j] == b) {
+                        System.out.println(j);
+                        continue outer;
+                    }
+                }
+                System.out.println(0);
+            } else if (command == 3) {
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+                for (int j = s; j <= e; j++) {
+                    System.out.print(arr[j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
