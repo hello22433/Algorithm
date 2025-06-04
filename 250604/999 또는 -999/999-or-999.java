5e35aaae947fefe39cfd28a3446b90991501d7c1
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 10:41:29 +0900

    Title: 999 또는 -999, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250604/999 또는 -999/999-or-999.java
+++ 250604/999 또는 -999/999-or-999.java
@@ -6,14 +6,14 @@ public class Main {
          
          int max = Integer.MIN_VALUE;
          int min = Integer.MAX_VALUE;
-        whlie (true) {
+        while (true) {
             int num = sc.nextInt();
             max = Math.max(num, max);
             min = Math.min(num, min);
 
             if (num == 999 || num == -999) {
                 System.out.print(max + " " + min);
-                break;
+                b
             }
         }
     }
