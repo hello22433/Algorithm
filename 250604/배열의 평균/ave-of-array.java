6be34f5819f2a9b7e6cafae3b7c1145528625cd6
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 14:50:16 +0900

    Title: 배열의 평균, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250604/배열의 평균/ave-of-array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr2d = new int[2][4];
+
+        for (int i = 0; i < 2; i++) {
+            // int sum = 0;
+            for (int j = 0; j < 4; j++) {
+                arr2d[i][j] = sc.nextInt();
+            }
+        }
+
+        // 가로평균
+        // 가로평균
+    }
+}
