b25509e9fd25cee3b18ebaa222a416974cb4c3f3
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 15:06:39 +0900

    Title: 배열의 평균, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

--- 250604/배열의 평균/ave-of-array.java
+++ 250604/배열의 평균/ave-of-array.java
@@ -6,14 +6,36 @@ public class Main {
 
         int[][] arr2d = new int[2][4];
 
+        // 가로평균
         for (int i = 0; i < 2; i++) {
-            // int sum = 0;
+            int sum = 0;
             for (int j = 0; j < 4; j++) {
                 arr2d[i][j] = sc.nextInt();
+                sum += arr2d[i][j];
+            }
+            System.out.printf("%.1f ", sum/(double)4);
+        }
+        System.out.println();
+
+        
+        // 세로평균
+        for (int i = 0; i < 4; i++) {
+            int sum = 0;
+            for (int j = 0; j < 2; j++) {
+                sum += arr2d[j][i];
+            }
+            System.out.printf("%.1f ", sum/(double)2);
+        }
+        System.out.println();
+        
+        // 전체평균
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                sum += arr2d[i][j];
             }
         }
+        System.out.printf("%.1f ", sum/(double)8);
 
-        // 가로평균
-        // 가로평균
     }
 }
