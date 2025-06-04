fb5eab16cac0348ae27317261132e92ef059d571
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 10:05:15 +0900

    Title: 코로나 메뉴얼2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250604/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = 0;
+        int B = 0;
+        int C = 0;
+        int D = 0;
+
+        for (int i = 0; i < 3; i++) {
+            String YN = sc.next();
+            int temper = sc.nextInt();
+
+            if (YN.equals("Y") && temper >= 37) {
+                A++;
+            } else if (temper >= 37) {
+                B++;
+            } else if (temper < 37) {
+                C++;
+            } else {
+                D++;
+            }
+        }
+
+        System.out.print(A + " " + B + " " + C + " " + D + " ");
+        if (A >= 2) System.out.print("E");
+    }
+}
