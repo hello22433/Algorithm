a6a20607dfdf7601e8c2ec2851553f656cea03f0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 14:25:36 +0900

    Title: 두 수의 차의 최솟값, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250604/두 수의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < n ; i++) {
+            for (int j = i+1; j < n; j++) {
+                minDiff = Math.min(minDiff, Math.abs(arr[j] - arr[i]));
+            }
+        }
+        System.out.print(minDiff);
+    }
+}
+
+
+// 가장 작은 수로 사 놓고 이후 가장 큰 수로 사야 한다.
+// 다만 가장 작지 않더라도 그 뒤의 수가 아주 크다면 이익이 생길 수 있다.
+// 즉, 가장 작은 수를 하나씩 지정하고 
