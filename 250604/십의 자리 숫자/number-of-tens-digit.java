48bc34da98638d84cf82c323c2b59733639517e5
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 09:58:48 +0900

    Title: 십의 자리 숫자, Time: 153ms, Memory: 10MB, Status: Passed - Codetree

--- 250604/십의 자리 숫자/number-of-tens-digit.java
+++ 250604/십의 자리 숫자/number-of-tens-digit.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int n = sc.nextInt();
         int[] countArr = new int[100];
 
         for (int i = 0; ; i++) {
