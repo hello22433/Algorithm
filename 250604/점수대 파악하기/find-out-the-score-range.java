1283cdd8a1257f92012ea9b715e9c645a2f34cdf
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-04 09:57:30 +0900

    Title: 점수대 파악하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250604/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] countArr = new int[101];
+
+        for (int i = 0; ; i++) {
+            int num = sc.nextInt();
+            if (num==0) break;
+            countArr[num/10]++; 
+        }
+
+        for (int i = 1; i<=9; i++) {
+            System.out.println(i+"0" + " - " + countArr[i]);
+        }
+    }
+}
