31fd81b3e9e7cb01f0f39c7271f8a56c0f280d8e
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-06 13:57:37 +0900

    Title: 지그재그로 정수 채우기, Time: 132ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250606/지그재그로 정수 채우기/zigzag-numbering.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 행크기
+        int n = sc.nextInt();
+        // 열크기
+        int m = sc.nextInt();
+
+        int[][] arr1 = new int[n][n];
+
+        int num = 0;
+        // 열
+        for (int i = 0; i < m; i++) {
+            // 행
+            if (i % 2 ==0) {
+                for (int j = 0; j < n; j++) {
+                    arr1[j][i] = num++;
+                }
+            } else {
+                for (int j = n-1; j >= 0; j--) {
+                    arr1[j][i] = num++;
+                }
+            }
+            
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
