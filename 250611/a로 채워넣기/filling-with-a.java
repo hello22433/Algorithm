f082cb32d575e7a63e407f83985a15a4d391e7f0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-11 23:27:48 +0900

    Title: a로 채워넣기, Time: 111ms, Memory: 10MB, Status: Passed - Codetree

--- 250611/a로 채워넣기/filling-with-a.java
+++ 250611/a로 채워넣기/filling-with-a.java
@@ -6,7 +6,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String a = sc.next();
+        char[] charArr = a.toCharArray();
+        charArr[1] = 'a';
+        charArr[charArr.length-2] = 'a';
+        System.out.print(String.valueOf(charArr));
 
-        System.out.print(a.substring(0,1) + "a" + a.substring(2, a.length()-2) + "a" + a.substring(a.length()-1));
+        // System.out.print(a.substring(0,1) + "a" + a.substring(2, a.length()-2) + "a" + a.substring(a.length()-1));
     }
 }
