83a9b74aab8990c9a05567c40a8afa1ede3b16b4
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-12 11:27:11 +0900

    Title: 아스키코드 변환, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250612/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        int b = sc.nextInt();
+
+        System.out.print((int)a.charAt(0) + " " + (char)b);
+    }
+}
