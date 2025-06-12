f139dfbe949e599e96a6dd16bf1a20e5e1786be0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-12 11:29:12 +0900

    Title: 그 다음 알파벳, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250612/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        int b = (int) a.charAt(0) + 1;
+        if (b > 122) b = 97;
+
+        System.out.print((char) b);
+    }
+}
