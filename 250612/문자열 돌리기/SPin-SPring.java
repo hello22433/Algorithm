7c6edb80e06c50a82d267b8a3cace57baa270a5d
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-12 11:14:26 +0900

    Title: 문자열 돌리기, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250612/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        int L = A.length();
+
+        for (int i = 0; i < L; i++) {
+            A = A.substring(A.length()-1,A.length()) + A.substring(0,A.length()-1);
+            System.out.println(A);
+        }
+    }
+}
