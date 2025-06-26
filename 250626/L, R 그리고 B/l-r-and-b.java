b00027d344845482a25dbe88e975676e382c043d
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-26 17:41:23 +0900

    Title: L, R 그리고 B, Time: 119ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250626/L, R 그리고 B/l-r-and-b.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[][] board = new char[10][10];
+        int LX = -1;
+        int LY = -1;
+        int BX = -1;
+        int BY = -1;
+        for (int i = 0; i < 10; i++) {
+            board[i] = sc.next().toCharArray();
+            for (int j = 0; j < 10; j++) {
+                if (board[i][j] == 'L') {
+                    LX = i;
+                    LY = j;
+                }
+                if (board[i][j] == 'B') {
+                    BX = i;
+                    BY = j;
+                }
+            }
+        }
+
+        System.out.print(Math.abs(LX-BX) + Math.abs(LY-BY) - 1);
+
+    }
+}
+
+// 문제
+// B에서 L로 가는 길에는 여러 최단 루트가 있다. 
+// 움직이는 길의 길이는 B와 L을 꼭짓점으로 하는 사각형에서 돌아가지만 않으면 일정하다.
+// R은 그 중의 한 루트만 막으므로 루트의 길이만을 구하여 출력하면 된다.
