a79a5252f63fd2988fc4bbab9d4449ec7f48b13d
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-07-02 22:11:15 +0900

    Title: 기수 정렬 구현, Time: 574ms, Memory: 24MB, Status: Wrong Answer - Codetree

---
+++ 250702/기수 정렬 구현/implement-radix-sort.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int strMaxLen = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            strMaxLen = Math.max(strMaxLen, String.valueOf(arr[i]).length());
+        }
+            
+
+        int[] radixedSortArr = radix_sort(arr, strMaxLen);
+
+        for (int i = 0; i < radixedSortArr.length; i++) {
+            System.out.print(radixedSortArr[i] + " ");
+        }
+    }
+
+    public static int[] radix_sort(int[] arr, int strMaxLen) {
+        
+        // 마지막 자리수부터 올라가면서 기수정렬을 한다.
+        for (int pos = strMaxLen-1; pos >= 0; pos--) {
+            ArrayList<Integer>[] sortedArrByDigit = new ArrayList[10];
+            for (int i = 0; i < 10; i++) {
+                sortedArrByDigit[i] = new ArrayList<Integer>();
+            }
+            
+            for (int i = 0; i < arr.length; i++) {
+                String strArrI = String.valueOf(arr[i]);
+
+                int arrI_posDigitValue = -1;
+                if (strArrI.length() < strMaxLen) {
+                    arrI_posDigitValue = 0;
+                } else {
+                    arrI_posDigitValue = Character.getNumericValue(strArrI.charAt(pos));
+                }
+                sortedArrByDigit[arrI_posDigitValue].add(arr[i]);
+            }
+
+            int[] tmpArr = new int[arr.length];
+            int tmpArrIdx = 0;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < sortedArrByDigit[i].size(); j++) {
+                    tmpArr[tmpArrIdx++] = sortedArrByDigit[i].get(j);
+                }
+            }
+
+            arr = tmpArr;
+        }
+
+        return arr;
+    }
+}
