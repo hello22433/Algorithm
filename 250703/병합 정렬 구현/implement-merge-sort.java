80514b5756d92e2551e403c4a8b273c21f1885d1
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-07-03 21:45:34 +0900

    Title: 병합 정렬 구현, Time: 1363ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

---
+++ 250703/병합 정렬 구현/implement-merge-sort.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        merge_sort(0, arr.length-1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void merge_sort(int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            merge_sort(low, mid);
+            merge_sort(mid+1, high);
+            merge(low, mid, high);
+        }
+    }
+
+    public static void merge(int low, int mid, int high) {
+        int[] merged_arr = new int[arr.length];
+        int i = low;
+        int j = mid + 1;
+        int k = low;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                merged_arr[k++] = arr[i++];
+            } else {
+                merged_arr[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            merged_arr[k++] = arr[i++];
+        }
+
+        while (j <= high) {
+            merged_arr[k++] = arr[j++];
+        }
+
+        for (int l = low; l <= high; l++) {
+            arr[l] = merged_arr[l];
+        }
+    } 
+}
