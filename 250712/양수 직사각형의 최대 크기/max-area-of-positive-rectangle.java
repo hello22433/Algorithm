5875c50c4a31c39d11b5e1df42719d6b3f214ebd
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-07-12 10:40:48 +0900

    Title: 양수 직사각형의 최대 크기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250712/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 250712/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -10,7 +10,7 @@ public class Main {
                 grid[i][j] = sc.nextInt();
         // Please write your code here.
 
-        int maxSize = 0;
+        int maxSize = -1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
