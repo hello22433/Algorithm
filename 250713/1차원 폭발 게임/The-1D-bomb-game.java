635146e05a11301e1eaed221176821d9840a3bfd
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-07-13 21:18:08 +0900

    Title: 1차원 폭발 게임, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250713/1차원 폭발 게임/The-1D-bomb-game.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++) {
+            bombs[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        boolean existBooms = true;
+
+        outer : while (existBooms) {
+            int firstBombVal = bombs[0];
+            int equalCnt = 1;
+            int startIdx = 0;
+            
+            for (int i = 1; i < n; i++) {
+                if (firstBombVal != bombs[i]) {
+
+                    if (equalCnt >= m) {
+                        // 터뜨리고 중력으로 내린다.
+                        int[] bucket = new int[n];
+                        int bucketIdx = 0;
+                        for (int j = 0; j < n; j++) {
+                            if ((j < startIdx || j > i-1) && bombs[j] != 0)
+                            bucket[bucketIdx++] = bombs[j];
+                        }
+                        for (int j = 0; j < n; j++) {
+                            bombs[j] = bucket[j];
+                        }
+
+                        continue outer;
+                    } else {
+                        firstBombVal = bombs[i];
+                        equalCnt = 1;
+                        startIdx = i;
+                    }
+                } else {
+                    equalCnt++;
+                }
+
+            }
+
+            existBooms = false;
+        }
+
+
+        int resultCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (bombs[i] != 0) resultCnt++;
+        }
+        System.out.println(resultCnt);
+        for (int i = 0; i < n; i++) {
+            if (bombs[i] != 0)
+                System.out.println(bombs[i]);
+        }
+    }
+}
+
+// N개 폭탄 중, M개가 연속이상이면 터진다. (연결된 모든 게 같은 수가 터져야 한다.)
+// 
+// 터지는게 없을 때까지 반복한다.
