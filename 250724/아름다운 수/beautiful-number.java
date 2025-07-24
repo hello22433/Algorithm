fa5126edb50167b8f2b382e2e54762d55a2853cd
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-07-24 23:59:50 +0900

    Title: 아름다운 수, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250724/아름다운 수/beautiful-number.java
+++ 250724/아름다운 수/beautiful-number.java
@@ -1,42 +1,58 @@
-import java.io.*;
 import java.util.*;
-
 
 public class Main {
     static int n;
     static int cnt = 0;
     static int[] arr;
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        dfs(0);
+        System.out.println(cnt);
+    }
 
-
-    static void func(int num){
-        if(num == n){
-            cnt++;
+    public static void dfs(int depth) {
+        if (depth == n) {
+            if (isBeautiful()) cnt++;
+            return;
         }
-        else{
-            for(int i = 1;i<=4;i++){
-                if(num+i > n){
-                     return;
-                }
-                else{
-                    for(int j = 0;j<i;j++){
-                        arr[num + j] = i;
-                    }
-                    func(num + i);
-                }
-            }
+
+        for (int i = 1; i <= 4; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
         }
     }
 
-    public static void main(String[] args) throws IOException{
-        // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static boolean isBeautiful() {
+        int[] count = new int[5]; // 1~4 숫자의 등장 개수
+        Set<Integer> used = new HashSet<>(); // 블록 중복 체크
+        int i = 0;
 
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        func(0);
-        System.out.println(cnt);
+        while (i < n) {
+            int cur = arr[i];
+            int j = i;
 
-        
+            // 같은 숫자가 끝날 때까지 탐색
+            while (j < n && arr[j] == cur) {
+                j++;
+            }
+
+            // 이 숫자가 예전에 다른 블록으로 등장한 적이 있다면 끊긴 것!
+            if (used.contains(cur)) return false;
+            used.add(cur);
+
+            count[cur] += (j - i); // 이번 블록 길이만큼 더함
+
+            i = j; // 다음 블록으로 이동
+        }
+
+        // 개수 조건 확인
+        if (count[2] % 2 != 0) return false;
+        if (count[3] % 3 != 0) return false;
+        if (count[4] % 4 != 0) return false;
+
+        return true;
     }
 }
