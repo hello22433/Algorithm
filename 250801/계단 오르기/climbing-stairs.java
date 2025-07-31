92445d46a3e3d908f8bf7a829a2e641c823aa966
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-08-01 01:23:33 +0900

    Title: 계단 오르기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 250801/계단 오르기/climbing-stairs.java
+++ 250801/계단 오르기/climbing-stairs.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
+
 public class Main {
-    static int n;
-    static int[] dp;
+    public static final int MOD = 10007;
+    public static int n;
+    public static int[] memo;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        // Please write your code here.
+        memo = new int[n + 1];
 
-
-
-        dp = new int[n+4];
-        dp[0] = 1;
-        memorization(0);
-
-        System.out.print(dp[n]);
-    }
-
-    public static void memorization(int num) {
-
-        if (num >= n){
-            return;
+        for (int i = 0; i <= n; i++) {
+            memo[i] = -1;  // 아직 계산 안 한 상태를 -1로 표시
         }
 
-        dp[num+2] += dp[num]%10007;
-        dp[num+3] += dp[num]%10007;
+        memo[0] = 1;  // 0까지 가는 방법은 1가지
+        if (n >= 1) memo[1] = 0;  // 1까지 가는 방법 없음
+        if (n >= 2) memo[2] = 1;  // 2까지 가는 방법 1가지
+        if (n >= 3) memo[3] = 1;  // 3까지 가는 방법 1가지
 
-        memorization(num+2);
-        memorization(num+3);
+        System.out.print(f(n));
+    }
+
+    public static int f(int x) {
+        if (x < 0) return 0;
+        if (memo[x] != -1) return memo[x];
+
+        memo[x] = (f(x - 2) + f(x - 3)) % MOD;
+        return memo[x];
     }
 }
