4eb9365840db97e7997a8bf8b2156b21d32191a0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-08-01 12:50:10 +0900

    Title: 사각형 채우기 3, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250801/사각형 채우기 3/rectangle-fill-3.java
+++ 250801/사각형 채우기 3/rectangle-fill-3.java
@@ -12,9 +12,9 @@ public class Main {
         if (n>=2) dp[2] = 7;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] += (dp[i-2]*3 + dp[i-1]*2) % 1000000007;
+            dp[i] = (dp[i-2]*3 + dp[i-1]*2) % 1000000007;
                 for (int j = i - 3; j >= 0; j--) {
-                    dp[i] += 2 * dp[j] % 1000000007;
+                    dp[i] = (dp[i] + 2 * dp[j]) % 1000000007;
                 }
         }
 
