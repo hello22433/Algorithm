1626dc74997bdaa62c519c7918ae9035b209be61
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-08-05 17:58:37 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 392ms, Memory: 17MB, Status: Passed - Codetree

--- 250805/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 250805/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -4,6 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] segments = new int[n][2];
+        int[] dp = new int[n];
         // int[] line = new int[1001];
 
         for (int i = 0; i < n; i++) {
@@ -11,30 +12,25 @@ public class Main {
             segments[i][1] = sc.nextInt();
         }
 
-        Arrays.sort(segments, (a,b) -> a[1]-b[1]);
+        Arrays.sort(segments, (a,b) -> a[0]-b[0]);
 
-        int selectLine = 1;
+        int maxLines = 1;
+        for (int i = 0; i < n; i++) {
 
-        int curEndPoint = segments[0][1];
+            dp[i] = 1;
 
-        for (int i = 1; i < n; i++) {
-            if (segments[i][0] > curEndPoint) {
-                curEndPoint = segments[i][1];
-                selectLine++;
+            for (int j = 0; j < i; j++) {
+                if (segments[j][1] < segments[i][0]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    maxLines = Math.max(maxLines, dp[i]);
+                }
             }
         }
-
-        System.out.print(selectLine);
+        
+        System.out.print(maxLines);
     }
 }
 
 
-// N개의 선분
-// 겹치지 않게 가장 많은 수의 선분을 고르시오.
-// 끝점 공유 : 겹친 것
-
-// 
-// 끝점을 기준으로 오름차순 정렬한다.
-// 처음부터 고르면서, 만약 시작점이 현재 끝점을 초과하면 끝점을 갱신하고, 
-// 선분 수를 +1 한다.
+// dp[i] : i 번째 선분을 마지막으로 했을 때, 겹치지 않는 가장 많은 수의 선분
 // 
