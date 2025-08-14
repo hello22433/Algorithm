98bfdecf6fb7b7913f8ffd7eb555b9840954c65a
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-08-14 17:44:02 +0900

    Title: 최소 차 분할, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250814/최소 차 분할/minimum-diff-partition.java
+++ 250814/최소 차 분할/minimum-diff-partition.java
@@ -1,44 +1,43 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        int totalSum = 0;
-        for (int i = 0; i < n; i++) {
+        int m = 0;
+        int[] arr = new int[n+1];
+        for (int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
-            totalSum += arr[i];
+            m += arr[i];
         }
+        // Please write your code here.
 
-        // dp[s] = 합 s를 만들 수 있는 경우, 사용한 원소 개수 저장
-        @SuppressWarnings("unchecked")
-        Set<Integer>[] dp = new Set[totalSum / 2 + 1];
-        for (int i = 0; i <= totalSum / 2; i++) {
-            dp[i] = new HashSet<>();
-        }
-        dp[0].add(0); // 합 0, 원소 개수 0
+        boolean[][] dp = new boolean[n+1][m+1];
 
-        // 부분합 DP
-        for (int num : arr) {
-            for (int s = totalSum / 2; s >= num; s--) {
-                for (int cnt : dp[s - num]) {
-                    dp[s].add(cnt + 1);
-                }
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 0; j <= m; j++) {
+                if (j-arr[i] >= 0 && dp[i-1][j-arr[i]]) dp[i][j] = true;
+                if (dp[i-1][j]) dp[i][j] = true;
             }
         }
 
-        int minDiff = Integer.MAX_VALUE;
-
-        // 가능한 s 중에서 최소 차이 계산
-        for (int s = 1; s <= totalSum / 2; s++) { // s=0은 한 그룹이 비어있으므로 제외
-            for (int cnt : dp[s]) {
-                if (cnt == 0 || cnt == n) continue; // 한쪽 그룹이 전부인 경우 제외
-                int diff = Math.abs(totalSum - 2 * s);
-                minDiff = Math.min(minDiff, diff);
+        int minVal = Integer.MAX_VALUE;
+        for (int i = 1; i <= m; i++) {
+            if (dp[n][i]) {
+                minVal = Math.min(minVal, Math.abs(i - (m - i)));
             }
         }
 
-        System.out.println(minDiff);
+        System.out.print(minVal);
     }
 }
+
+
+// 합을 만들 것이고, 
+// i번 째 수까지 고려했을 때, 합이 j가 된다면, j - (m - j) 가 그 차가 된다.
+// 하나의 합만 구하면 된다. 
+// dp[i][j] : i번째수까지 고려했을 때, j가 될 수 있는지?
+// dp[0][0] 가능
