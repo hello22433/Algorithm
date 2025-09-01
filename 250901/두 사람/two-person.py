0582b6f3b0d12dd731657ab49acfe0450cb1f7ae
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-09-01 22:43:37 +0900

    Title: 두 사람, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250901/두 사람/two-person.py
+++ 250901/두 사람/two-person.py
@@ -1,7 +1,10 @@
 aage, asex = input().split()
 bage, bsex = input().split()
 
-if (aage=="1" and asex=='M') or (bage=="1" and bsex=='M') :
+aage = int(aage)
+bage = int(bage)
+
+if (aage>=19 and asex=='M') or (bage>=19 and bsex=='M') :
     print(1)
 else :
     print(0)
