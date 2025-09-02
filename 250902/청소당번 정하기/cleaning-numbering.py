12f9898ed59badea7dcb8899a096cfd55b6fb776
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-09-02 12:36:54 +0900

    Title: 청소당번 정하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/청소당번 정하기/cleaning-numbering.py
+++ 250902/청소당번 정하기/cleaning-numbering.py
@@ -6,11 +6,11 @@ toilet = 0
 
 for i in range(1,n+1) :
 
-    if (n%12==0) :
+    if (i%12==0) :
         toilet+=1
-    elif n%3==0:
+    elif i%3==0:
         street+=1
-    elif n%2==0:
+    elif i%2==0:
         classroom+=1
 
 print(classroom,street,toilet)
