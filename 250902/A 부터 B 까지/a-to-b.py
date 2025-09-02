dc754f6231711fbbd98cf6222baf57a1010aba21
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-09-02 12:14:05 +0900

    Title: A 부터 B 까지, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/A 부터 B 까지/a-to-b.py
+++ 250902/A 부터 B 까지/a-to-b.py
@@ -1,9 +1,8 @@
 a,b=map(int,input().split())
 
-for i in range(a,b+1,1) :
-    print(i, end=" ")
-    if i%2==1:
-        i*=2
+while a<=b:
+    print(a, end=" ")
+    if a%2==1:
+        a*=2
     else:
-        i+=3
-    i-=1
+        a+=3
