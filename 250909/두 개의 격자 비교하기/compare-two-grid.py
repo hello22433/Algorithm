200a22bb949065309248d2481004198fdae293c0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-09-09 19:20:39 +0900

    Title: 두 개의 격자 비교하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250909/두 개의 격자 비교하기/compare-two-grid.py
+++ 250909/두 개의 격자 비교하기/compare-two-grid.py
@@ -7,7 +7,7 @@ new_arr = [[0 for _ in range(m)] for _ in range(n)]
 
 for i in range(n) :
     for j in range(m) :
-        if arr1[i] == arr2[j] :
+        if arr1[i][j] == arr2[i][j] :
             new_arr[i][j] = 0
         else :
             new_arr[i][j] = 1
