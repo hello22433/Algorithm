7446df4f2a52cfe17810311f40b1eeb4b1762851
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-09-23 10:19:53 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250923/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+# Please write your code here.
+
+def pro(n) :
+    if n==1:
+        return 0
+
+    if n%2==0 :
+        return pro(n//2) + 1
+    else :
+        return pro(n*3+1) + 1
+
+print(pro(n))
