9a2b9cf5131983a4c8d1b88ae0be8712aa04d434
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-09-24 14:23:43 +0900

    Title: 키를 기준으로 정렬, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250924/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,25 @@
+n = int(input())
+name = []
+height = []
+weight = []
+
+class S :
+    def __init__(self,n,h,w):
+        self.n = n
+        self.h = h
+        self.w = w
+
+arr = []
+
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+
+    arr.append(S(n_i,h_i,w_i))
+
+arr.sort(key=lambda x: x.h)
+
+for e in arr :
+    print(e.n,e.h,e.w)
