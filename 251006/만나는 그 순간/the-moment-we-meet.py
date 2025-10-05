6c5e1c666e9252f1d5c1172c19031c034eb6089a
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-06 01:48:15 +0900

    Title: 만나는 그 순간, Time: 103ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 251006/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,48 @@
+n, m = map(int, input().split())
+
+aArr = []
+bArr = []
+
+d = []
+t = []
+aLoc = 0
+bLoc = 0
+
+for _ in range(n):
+    direction, time = input().split()
+    d.append(direction)
+    t.append(int(time))
+
+    for i in range(int(time)) :
+        if direction == 'L' :
+            aLoc -= 1
+        else :
+            aLoc += 1
+        aArr.append(aLoc)
+
+d2 = []
+t2 = []
+for _ in range(m):
+    direction, time = input().split()
+    d2.append(direction)
+    t2.append(int(time))
+
+    for i in range(int(time)) :
+        if direction == 'L' :
+            bLoc -= 1
+        else :
+            bLoc += 1
+        bArr.append(bLoc)
+
+check = True
+for i in range(len(aArr)) :
+    if aArr[i] == bArr[i] :
+        print(i+1)
+        check = False
+        break
+
+if check :
+    print(-1)
+
+# Please write your code here.
+
