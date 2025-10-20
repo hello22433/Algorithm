e0d45104f83be8f7f67daf9fb2f51eb0ff1e65c9
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-21 00:20:24 +0900

    Title: 되돌아오기 2, Time: 86ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251021/되돌아오기 2/come-back-2.py
@@ -0,0 +1,32 @@
+commands = input()
+
+# Please write your code here.
+
+dir = 3
+
+# 동 남 서 북
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+curX = 0
+curY = 0 
+
+cnt = 0
+
+for command in commands :
+    if command == 'L' :
+        dir = (dir - 1 + 4) % 4 
+    elif command == 'R' :
+        dir = (dir + 1) % 4
+    else :
+        curX += dx[dir]
+        curY += dy[dir]
+    cnt += 1
+
+    if curX == 0 and curY == 0 :
+        check = True
+        print(cnt)
+        break
+
+if not check :
+    print(-1)
