cc212d8824d31c16d00d73f7d1a4d4b7c96edadb
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-21 10:48:53 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 74ms, Memory: 18MB, Status: Passed - Codetree

--- 251021/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 251021/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -3,28 +3,28 @@ grid = [[0] * n for _ in range(n)]
 
 # Please write your code here.
 
-curX = n // 2
-curY = n // 2
+curX = n - 1
+curY = n - 1
 
 # 동남서북
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
-dir = 0
+dir = 2
 
-grid[curX][curY] = 1
+grid[curX][curY] = n*n
 
 def inRange(x,y) :
     if x>=0 and x<n and y>=0 and y<n :
         return True
     return False
 
-for i in range(2,n*n+1) :
+for i in range(n*n-1,0,-1) :
     nx = curX + dx[dir]
     ny = curY + dy[dir]
 
     if not inRange(nx,ny) or grid[nx][ny] != 0 :
-        dir = (dir - 1 + 4) % 4
+        dir = (dir + 1) % 4
         nx = curX + dx[dir]
         ny = curY + dy[dir]
 
