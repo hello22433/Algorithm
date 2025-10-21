b4ae450894cb897462f0ea5107b38c12d2dad401
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-22 01:13:24 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251022/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,15 @@
+A = input()
+
+# Please write your code here.
+cnt = 0
+for i in range(len(A)) :
+    if A[i] != '(' :
+        continue
+
+    for j in range(i+1, len(A)) :
+        if A[j] == ')' :
+            cnt += 1
+
+print(cnt)
+
+        
