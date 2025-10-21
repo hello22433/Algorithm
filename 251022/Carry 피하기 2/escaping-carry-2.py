eb0fcf76402d08f2938f47c64409ad64caa670e1
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-22 02:35:13 +0900

    Title: Carry 피하기 2, Time: 64ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251022/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+max_sum =0
+
+def occur_sum_carry(num1,num2) :
+    while num1 != 0 and num2 != 0 :
+        if num1%10 + num2%10 >= 10 :
+            return True
+        num1 //= 10
+        num2 //= 10
+    return False
+
+for i in range(n) :
+    for j in range(n) :
+        for k in range(n) :
+            if not occur_sum_carry(arr[i],arr[j]) and not occur_sum_carry((arr[i]+arr[j]),arr[k]) :
+                max_sum = max(max_sum,arr[i]+arr[j]+arr[k])
+
+print(max_sum)
