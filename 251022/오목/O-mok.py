194cea1fc844bddda014a27db7a413de88a82443
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-22 02:59:20 +0900

    Title: 오목, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251022/오목/O-mok.py
+++ 251022/오목/O-mok.py
@@ -37,11 +37,11 @@ for i in range(4,19) :
     for j in range(19-5) :
         if (board[i][j]==1 and board[i-1][j+1]==1 and board[i-2][j+2]==1 and board[i-3][j+3]==1 and board[i-4][j+4]==1) :
             result=1
-            x = i+2+1
+            x = i-2+1
             y = j+2+1
         elif (board[i][j]==2 and board[i-1][j+1]==2 and board[i-2][j+2]==2 and board[i-3][j+3]==2 and board[i-4][j+4]==2) :
             result=2
-            x = i+2+1
+            x = i-2+1
             y = j+2+1
 
 print(result)
