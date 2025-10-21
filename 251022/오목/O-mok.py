b6c830a96e06ebf995200d20d63e1fe521c0efb3
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-22 03:02:39 +0900

    Title: 오목, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251022/오목/O-mok.py
+++ 251022/오목/O-mok.py
@@ -7,6 +7,28 @@ x = 0
 y = 0
 
 for i in range(19-5) :
+    for j in range(19) :
+        if (board[i][j]==1 and board[i+1][j]==1 and board[i+2][j]==1 and board[i+3][j]==1 and board[i+4][j]==1) :
+            result=1
+            x = i+2+1
+            y = j+1
+        elif (board[i][j]==2 and board[i+1][j]==2 and board[i+2][j]==2 and board[i+3][j]==2 and board[i+4][j]==2) :
+            result=2
+            x = i+2+1
+            y = j+1
+
+for i in range(19) :
+    for j in range(19-5) :
+        if (board[i][j]==1 and board[i][j+1]==1 and board[i][j+2]==1 and board[i][j+3]==1 and board[i][j+4]==1) :
+            result=1
+            x = i+1
+            y = j+2+1
+        elif (board[i][j]==2 and board[i][j+1]==2 and board[i][j+2]==2 and board[i][j+3]==2 and board[i][j+4]==2) :
+            result=2
+            x = i+1
+            y = j+2+1
+
+for i in range(19-5) :
     for j in range(19-5) :
         if (board[i][j]==1 and board[i+1][j]==1 and board[i+2][j]==1 and board[i+3][j]==1 and board[i+4][j]==1) :
             result=1
