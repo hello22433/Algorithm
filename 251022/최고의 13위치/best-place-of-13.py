3ff05cc018c401a2caeca731f58361ca6595b018
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-22 01:19:30 +0900

    Title: 최고의 13위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251022/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,11 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+max_sum = 0
+for i in range(n) :
+    for j in range(n-2) :
+        max_sum = max(max_sum, grid[i][j] + grid[i][j+1] + grid[i][j+2])
+
+print(max_sum)
