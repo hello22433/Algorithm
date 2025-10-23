bfae86dd091967068ed31574fe19fc97e1827194
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-24 00:46:52 +0900

    Title: 아름다운 수열 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251024/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+B.sort()
+
+# Please write your code here.
+
+cnt = 0
+if M>N :
+    print(0)
+else :
+    for i in range(N-(M-1)) :
+        s = []
+
+        for j in range(i,i+M) :
+            s.append(A[j])
+        s.sort()
+        if s == B :
+            cnt += 1
+print(cnt)
