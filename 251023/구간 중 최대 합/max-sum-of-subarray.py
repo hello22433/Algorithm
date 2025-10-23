b2f674c307af151e0eeddd6bdcab579e7ebb25b0
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-10-23 23:28:17 +0900

    Title: 구간 중 최대 합, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251023/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+max_sum = 0
+for i in range(n-k+1) :
+    sum = 0
+    for j in range(i,i+k) :
+        sum += arr[j]
+
+    max_sum = max(max_sum,sum)
+
+print(max_sum)
