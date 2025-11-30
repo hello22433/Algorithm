bbfbdebc75b6dbb90fa84134408608dd76161a8c
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-11-30 20:46:09 +0900

    Title: 고대 보물 지도의 비밀, Time: 143ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251130/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -0,0 +1,82 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long globalMaxScore = 0;
+
+        long[] dp = new long[K + 1];
+        long INF = -100000000L;
+        Arrays.fill(dp, INF);
+
+        for (int i = 0; i < N; i++) {
+
+            long curNum = arr[i];
+            
+            // 만약 양수라면 그냥 더하면 된다.
+            // 음수의 개수가 0인 경우는 이 수부터 다시 시작할지 선택 가능하다.
+
+            if (curNum > 0) {
+
+                for (int k = 0; k <= K; k++) {
+                    if (dp[k] == INF) {
+                        dp[k] = curNum;
+                    } else {
+                        dp[k] = dp[k] + curNum;
+                    }
+
+                    globalMaxScore = Math.max(dp[k], globalMaxScore);
+                }
+            } else {
+
+                // 음수라면, 
+                // 음수를 하나씩 민다.
+                // 즉, dp[1]은 dp[2]가 된다. 여기서 한 사이클에서 값이 누적되지 않게 거꾸로 계산한다.
+                // 0인 경우는 1이 된다. 
+
+                for (int k = K; k >= 1; k--) {
+                    
+                    
+                    if (dp[k-1] != INF) {
+                        dp[k] = Math.max(dp[k-1] + curNum, curNum);
+                    } else {
+
+                    }
+
+                    
+                    globalMaxScore = Math.max(dp[k], globalMaxScore);
+
+                    
+                }
+                dp[0] = INF;
+                
+            }
+
+            
+        }
+
+        System.out.print(globalMaxScore);
+        
+    }
+}
+
+// 음수의 개수 k
+// dp[k] : k개의 음수일 때 현재 최대값
+// 카데인 알고리즘 + dp
+// 
+// 
