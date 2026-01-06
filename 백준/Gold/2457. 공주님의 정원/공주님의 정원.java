fc49051d4802b9f08266aa70b650ee3c920bb80d
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-01-06 19:58:59 +0900

    [Gold III] Title: 공주님의 정원, Time: 1168 ms, Memory: 310116 KB -BaekjoonHub

---
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Flower {
+    int start;
+    int end;
+    
+    Flower(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        Flower[] flowers = new Flower[n];
+        
+        for (int i = 0; i < n; i++) {
+            int sm = sc.nextInt();
+            int sd = sc.nextInt();
+            int em = sc.nextInt();
+            int ed = sc.nextInt();
+            
+            int startMD = sm * 100 + sd;
+            int endMD = em * 100 + ed;
+            
+            flowers[i] = new Flower(startMD, endMD);
+            
+        }
+        
+        Arrays.sort(flowers, (a,b) -> {
+           if (a.start == b.start) return b.end - a.end;
+           return a.start - b.start;
+        });
+        
+        final int START = 301;
+        final int END = 1130;
+        
+        int idx = 0; // 꽃 배열 포인터
+        int count = 0; // 선택한 꽃 수
+        int cur = START; // 현재 덮어야하는 날짜
+        
+        while (cur <= END) {
+            
+            int maxEnd = cur;
+            
+            while (idx < n && flowers[idx].start <= cur) {
+                maxEnd = Math.max(maxEnd, flowers[idx].end);
+                idx++;
+            }
+            
+            if (maxEnd == cur) {
+                System.out.print(0);
+                return;
+            }
+            
+            cur = maxEnd;
+            count++;
+        }
+        
+        System.out.print(count);
+    }
+}
