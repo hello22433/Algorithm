31478a51ba6dbacdda44a82cc7095810c52e4401
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-01-13 17:23:13 +0900

    [Silver I] Title: 팔, Time: 164 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.java
+++ 백준/Silver/1105. 팔/팔.java
@@ -3,32 +3,26 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        // 처음부터 문자열로 입력받기 (20억이라는 큰 수 대응)
         String LS = sc.next();
         String RS = sc.next();
-
+        
         int result = 0;
-
-        // 1. 자릿수가 다르면 무조건 0 (예: 88과 100 사이엔 90이 있음)
         if (LS.length() != RS.length()) {
             System.out.print(0);
             return;
         }
-
-        // 2. 자릿수가 같을 때만 앞에서부터 비교
+        
         for (int i = 0; i < LS.length(); i++) {
-            // 같은 자리에 둘 다 8이 있으면 무조건 포함될 수밖에 없음
             if (LS.charAt(i) == RS.charAt(i)) {
-                if (LS.charAt(i) == '8') {
-                    result++;
+                if (RS.charAt(i) == '8') {
+                    
+                    result += 1;
                 }
             } else {
-                // 숫자가 달라지는 순간부터는 8이 없는 숫자를 만들 수 있음
                 break;
             }
         }
-
+        
         System.out.print(result);
     }
 }
