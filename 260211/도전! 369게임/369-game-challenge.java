8b1e701fa52f30bbff2a323c0bfab7ab0beff6cf
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-11 22:39:27 +0900

    Title: 도전! 369게임, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260211/도전! 369게임/369-game-challenge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        // Please write your code here.
+
+        for (long i = 0; i < Long.parseLong(n); i++) {
+            
+        }
+    }
+}
+
+// 지
