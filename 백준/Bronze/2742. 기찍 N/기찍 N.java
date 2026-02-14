4568e8c67c695bef9b8cbab510bda20aa4295dcb
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-14 15:30:25 +0900

    [Bronze V] Title: 기찍 N, Time: 812 ms, Memory: 34048 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+//        String s = sc.nextLine();
+        
+        for (int i = c; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+
+
+
+
+
+    }
+}
