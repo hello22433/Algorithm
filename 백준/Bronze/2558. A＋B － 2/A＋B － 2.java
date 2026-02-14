dddc50e5dc4406e5ee82b8730c814c73ee1246a2
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-14 17:02:43 +0900

    [Bronze V] Title: A+B - 2, Time: 152 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        String s = sc.nextLine();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        System.out.println(a+b);
+    }
+
+}
+
+// 매핑하는식
+// 3으로 나눈 몫으로 가는 식 (문자가 한 숫자에 4개인 경우는?)
+//
