fc168312e4533c0b7f29160ac22763a608ecd6d1
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-14 14:58:30 +0900

    [Bronze V] Title: We love kriii, Time: 148 ms, Memory: 17016 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+
+
+
+
+
+
+    }
+}
