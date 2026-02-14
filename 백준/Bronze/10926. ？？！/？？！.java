9006ca7ad8cee0c0347f4fe4445292b73b14663a
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-14 14:58:27 +0900

    [Bronze V] Title: ??!, Time: 148 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        int n = sc.nextInt();
+        String s = sc.next();
+
+        System.out.print(s + "??!");
+
+
+
+
+
+
+    }
+}
