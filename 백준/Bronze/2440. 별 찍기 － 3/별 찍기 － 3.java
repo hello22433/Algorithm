e9b41e47196ac77e98b580b43a52ce7a03232755
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-15 13:27:30 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 248 ms, Memory: 18076 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+// 매핑하는식
+// 3으로 나눈 몫으로 가는 식 (문자가 한 숫자에 4개인 경우는?)
+//
