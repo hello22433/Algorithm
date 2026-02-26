54640b32d620b96e2a1425150965957d15eae69e
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-26 21:51:37 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 196 ms, Memory: 18072 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+//        st = new StringTokenizer(br.readLine());
+//
+//        int x = Integer.parseInt(st.nextToken());
+//        int y = Integer.parseInt(st.nextToken());
+//        int z = Integer.parseInt(st.nextToken());
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+
+
+
+
+
+
+    }
+}
