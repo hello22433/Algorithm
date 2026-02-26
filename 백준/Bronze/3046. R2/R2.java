9a4dd6bc297398065fd4c94651539691dd67c854
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-02-26 20:57:00 +0900

    [Bronze IV] Title: R2, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+//        st = new StringTokenizer(br.readLine());
+//
+//        int x = Integer.parseInt(st.nextToken());
+//        int y = Integer.parseInt(st.nextToken());
+//        int z = Integer.parseInt(st.nextToken());
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s2 = sc.nextInt();
+        
+        int r2 = s2 * 2 - r1;
+        
+        System.out.println(r2);
+
+
+
+
+
+
+    }
+}
