e15e2a52e6f070daddc6753a30673a7e9efe454b
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-08-07 19:32:00 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, date_of_birth
+from member_profile
+where date_of_birth LIKE '%-03-%' and tlno is not null and gender = 'W'
+order by member_id ASC;
