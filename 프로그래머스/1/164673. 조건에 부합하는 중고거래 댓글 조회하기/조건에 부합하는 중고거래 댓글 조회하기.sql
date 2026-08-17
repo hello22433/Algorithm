c1879d9c546614154bf2cf4b6db49636cee56556
hello22433 <104301546+hello22433@users.noreply.github.com>
2026-08-17 19:58:57 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT title, r.board_id, reply_id, r.writer_id, r.contents, r.created_date
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, r.created_date
 from used_goods_board b
 inner join used_goods_reply r on b.board_id = r.board_id
 where b.created_date > '2022-09-30' and b.created_date < '2022-11-01'
-order by r.created_date ASC, b.TITLE ASC;
+order by r.created_date asc, b.title asc;
